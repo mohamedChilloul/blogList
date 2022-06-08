4acb129800c3c48c7579d6409c1e92f9c70ad106
mohamedChilloul <m.chilloul@esi-sba.dz>
2022-06-08 16:22:05 +0100

    adding backend tests Steps: 1 - 2 - 3 - 4 - 5

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,17 +1,21 @@
 const Blog = require('../models/blog')
 const blogRouter = require('express').Router()
+require('express-async-errors')
 
-blogRouter.get('/', (req, response)=>{
-    Blog.find({}).then(res=>{
-        response.json(res)
-    })
+blogRouter.get('/',async (req, response)=>{
+    const blogs = await Blog.find({})
+    response.json(blogs)
 })
 
-blogRouter.post('/', (req,response)=>{
-    newBlog = new Blog({...req.body})
-    newBlog.save().then(res=>{
-        response.json(res)
-    }).catch(err =>console.log(err.message))
+blogRouter.post('/',async (req,response)=>{
+    const newBlog = new Blog({...req.body})
+    if (!newBlog.title || !newBlog.url){
+        response.status(400).end()
+    }else{
+        const savedBlog = await newBlog.save()
+        response.status(201).json(savedBlog)
+    }
+   
 })
 
 module.exports = blogRouter
